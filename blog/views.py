617a605500f248dd2fb48e7a736f72693c361d31
Николай Самсонов <samsonovn11@gmail.com>
2021-04-10 23:06:30 +0300

    Add blog page with posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . models import Post
 
 # Create your views here.
+def showblog(requests):
+    posts = Post.objects
+    return render(requests, 'blog/blog.html', {'posts': posts})
