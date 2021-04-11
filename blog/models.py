ff1ab28393170f3ef29feaf51257711be80f5dae
Николай Самсонов <samsonovn11@gmail.com>
2021-04-11 20:56:46 +0300

    End of development MAB

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,8 @@ class Post(models.Model):
     text = models.TextField()
     image = models.ImageField(upload_to='event_images/')
 
+    def get_summary(self):
+        return self.text[:70]
+
+    def __str__(self):
+        return self.title
